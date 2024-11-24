04790d31cc38df5499a15c6e616699160e972987
zetterus <zetter.vitrolic@gmail.com>
2024-11-24 18:55:22 +0200

    first three tasks done

--- multiple_sites_from_excel.py
+++ multiple_sites_from_excel.py
@@ -1,10 +1,8 @@
 import pandas as pd
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 # Чтение Excel-файла
